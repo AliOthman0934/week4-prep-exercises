53065f83a08848dbb1b1534c4525d099995c3311
Ali Othman <alioth840@gmail.com>
2023-11-25 21:19:36 +0100

    week4 prep exercises

---
+++ ex3-object.js
@@ -0,0 +1,79 @@
+import eurosFormatter from './euroFormatter.js';
+
+function createWallet(name, cash = 0) {
+  return {
+    _name: name,
+    _cash: cash,
+    _dailyAllowance: Infinity, 
+
+    deposit: function (amount) {
+      this._cash += amount;
+    },
+
+    withdraw: function (amount) {
+      if (this._cash - amount < 0) {
+        console.log(`Insufficient funds!`);
+        return 0;
+      }
+
+      
+      if (amount > this._dailyAllowance) {
+        console.log(`Withdrawal exceeds the daily allowance!`);
+        return 0;
+      }
+
+      this._cash -= amount;
+      return amount;
+    },
+
+    transferInto: function (wallet, amount) {
+      console.log(
+        `Transferring ${eurosFormatter.format(amount)} from ${
+          this._name
+        } to ${wallet.getName()}`
+      );
+      const withdrawnAmount = this.withdraw(amount);
+      wallet.deposit(withdrawnAmount);
+    },
+
+    reportBalance: function () {
+      console.log(
+        `Name: ${this._name}, balance: ${eurosFormatter.format(this._cash)}`
+      );
+    },
+
+    getName: function () {
+      return this._name;
+    },
+
+    setDailyAllowance: function (newAllowance) {
+      this._dailyAllowance = newAllowance;
+    },
+  };
+}
+
+function main() {
+  const walletJack = createWallet('Jack', 100);
+  const walletJoe = createWallet('Joe', 10);
+  const walletJane = createWallet('Jane', 20);
+
+  walletJack.transferInto(walletJoe, 50);
+  walletJane.transferInto(walletJoe, 25);
+
+  walletJane.deposit(20);
+  walletJane.transferInto(walletJoe, 25);
+
+  walletJack.reportBalance();
+  walletJoe.reportBalance();
+  walletJane.reportBalance();
+
+  
+  walletJack.setDailyAllowance(75);
+
+  const withdrawnAmount = walletJack.withdraw(80);
+  console.log(`Withdrawn Amount: ${eurosFormatter.format(withdrawnAmount)}`);
+  walletJack.reportBalance();
+}
+
+main();
+
